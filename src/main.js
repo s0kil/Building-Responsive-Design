5cbf36b1e0b06c74b15defb8cc68e4e44cbf4450
Daniel Sokil <dtechtours@gmail.com>
2019-12-27 17:33:37 -0500

    Sticky Nav Bar On Scroll & Beginning Of Responsive Design

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
-let categoriesListItem = [...document.getElementsByClassName("category-item-list-popular")];
-categoriesListItem.forEach(listItem => {
-  let categoryItemBgimage = listItem.dataset.categoryItemBgimage;
-  if (categoryItemBgimage !== undefined) {
-    listItem.style.backgroundImage = `linear-gradient(
+(() => {
+  // Load Category List Images
+  let categoriesListItem = [...document.getElementsByClassName("category-item-list-popular")];
+  categoriesListItem.forEach(listItem => {
+    let categoryItemBgImage = listItem.dataset.categoryItemBgimage;
+    if (categoryItemBgImage !== undefined) {
+      listItem.style.backgroundImage = `linear-gradient(
       to bottom,
       rgba(0, 0, 0, 0.4) 50%,
       rgba(0, 0, 0, 0.8),
       rgba(0, 0, 0, 1)
-      ), url(${categoryItemBgimage})`;
+      ), url(${categoryItemBgImage})`;
+    }
+  });
+
+  let nav = document.querySelector("nav");
+  if (screen.width > 320) {
+    // Nav Bar Sticky On Scroll
+    window.onscroll = () => {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.id = "nav-sticky";
+      } else nav.id = "";
+    };
+  } else {
+    nav.id = "nav-sticky";
   }
-});
+})();
