156f7a5f8003f2f5b530425a5588443f1b5a0ca1
Daniel Sokil <dtechtours@gmail.com>
2020-01-06 14:04:49 -0500

    Remove Includes Functionality

--- src/main.js
+++ src/main.js
@@ -1,37 +1,8 @@
 (() => {
-  // Wait for HTML document to been completely loaded and parsed
-  window.addEventListener("DOMContentLoaded", () => {
-    let includes = [...document.getElementsByTagName("include")];
-    includes.forEach((include, index) => {
-      let src = include.getAttribute("src");
-      fetch(src, { mode: "no-cors" })
-        .then(response => {
-          if (!response.ok) throw Error(response.statusText);
-          return response.text();
-        })
-        .then(html => {
-          include.innerHTML = html || "";
-        })
-        .catch(error => {
-          console.error(`Failed To Load <include src="${src}" />`);
-          console.error(error);
-        })
-        .finally(() => {
-          // If Last Include Is Loaded
-          if (index === includes.length - 1) {
-            main(); // Call main function
-          }
-        });
-    });
-  });
-
-  function scrollPositionY() {
-    return document.body.scrollTop || document.documentElement.scrollTop;
-  }
+  // Wait for HTML document to been completely loaded and parsed, then call main function
+  window.addEventListener("load", main);
 
   function main() {
-    restoreScrollPosition();
-
     // Load Category List Images
     let categoriesListItem = [
       ...document.getElementsByClassName("category-item-list-popular")
@@ -47,33 +18,15 @@
     if (screen.width > 320) {
       // Nav Bar Sticky On Scroll
       window.addEventListener("scroll", () => {
-        if (scrollPositionY() > 20) {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
           nav.id = "nav-sticky";
         } else nav.id = "";
       });
     } else {
       nav.id = "nav-sticky";
     }
-
-    saveScrollPosition();
-  }
-
-  let timeout;
-  function saveScrollPosition() {
-    window.addEventListener(
-      "scroll",
-      () => {
-        if (timeout) window.cancelAnimationFrame(timeout);
-        timeout = window.requestAnimationFrame(() => {
-          localStorage.setItem("scrollPosition", scrollPositionY());
-        });
-      },
-      { passive: true }
-    );
-  }
-
-  function restoreScrollPosition() {
-    let scrollPosition = Number(localStorage.getItem("scrollPosition") || 0);
-    if (scrollPosition) window.scrollTo(0, scrollPosition);
   }
 })();
