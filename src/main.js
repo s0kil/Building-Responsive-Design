e6c1fdf0d18c168422f597979705bcc37a09b7c8
Daniel Sokil <dtechtours@gmail.com>
2020-01-07 15:13:11 -0500

    Inline Background Images Into HTML

--- src/main.js
+++ src/main.js
@@ -1,19 +1,8 @@
 (() => {
-  // Wait for HTML document to been completely loaded and parsed, then call main function
+  // Wait for HTML document to been completely loaded, then call main function
   window.addEventListener("load", main);
 
   function main() {
-    // Load Category List Images
-    let categoriesListItem = [
-      ...document.getElementsByClassName("category-item-list-popular")
-    ];
-    categoriesListItem.forEach(listItem => {
-      let categoryItemBgImage = listItem.dataset.categoryItemBgImage;
-      if (categoryItemBgImage !== undefined) {
-        listItem.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, 0.4) 50%, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 1)), url(${categoryItemBgImage})`;
-      }
-    });
-
     let nav = document.querySelector("nav");
     if (screen.width > 320) {
       // Nav Bar Sticky On Scroll
